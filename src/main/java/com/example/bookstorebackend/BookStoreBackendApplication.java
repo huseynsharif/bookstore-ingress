46edb3ae2d2cc655818a608ced3f2a7c9a60e2af
Huseyn Sharifzade <huseynsharif@gmail.com>
2023-11-02 16:59:47 +0400

    Initial commit

---
+++ src/main/java/com/example/bookstorebackend/BookStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreBackendApplication.class, args);
+    }
+
+}
